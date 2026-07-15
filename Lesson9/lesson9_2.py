06039981f5d21e5b5bd6a38181a3f40124b39eed
pien0609 <pien0609@gmail.com>
2026-07-15 21:46:00 +0800

    上課用

---
+++ Lesson9/lesson9_2.py
@@ -0,0 +1,14 @@
+from playwright.sync_api import sync_playwright
+from playwright.sync_api import Browser,Page
+
+def main():
+  with sync_playwright() as p:
+     browser:Browser =p.chromium.launch(headless=False)
+     page: Page=browser.new_page()
+     page.goto("https://www.google.com")
+     print(page.title())
+     browser.close()
+
+
+if __name__== "__main__":
+  main()
